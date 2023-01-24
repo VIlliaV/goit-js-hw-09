6fc10e2efb58fe42669c2f212b3ad2cd292a61f6
Illia <illia.vasiliev@gmail.com>
2023-01-24 17:35:01 +0200

    start 9.3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,29 @@
+const { delay } = require('lodash');
+
+const formEl = document.querySelector('.form');
+
+formEl.addEventListener('input', inputF);
+formEl.addEventListener('submit', submitF);
+console.log(formEl);
+function inputF(e) {
+  console.log(e.target);
+}
+function submitF(e) {
+  e.preventDefault();
+  const {
+    elements: { delay, step, amount },
+  } = e.target;
+
+    console.log(+delay.value, +step.value);
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+    resolve('success value');
   } else {
     // Reject
+    reject('error');
   }
 }
